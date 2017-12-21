dae62de592a338970604ce62d63cbe08111aedca
Mohamed Ben Henda <m.benhenda@viasyst.com>
2017-12-21 15:17:44 +0100

    Changes stars to pluses

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -8,24 +8,20 @@ package com.wakaleo.gameoflife.domain;
  * Cell object class, contains information on individual cells (positions) in the grid
  */
 public enum Cell {
-	// Symbols to represent cell status
-    LIVE_CELL("*"), DEAD_CELL(".");
+    LIVE_CELL("+"), DEAD_CELL(".");
 
     private String symbol;
 
-    private Cell(final String initialSymbol) {
-        this.symbol = initialSymbol;
+    private Cell(String symbol) {
+        this.symbol = symbol;
     }
 
-	// Override function for easy printing of cell's symbol
-	// Functions identically to default getter ( getSymbol() )
     @Override
     public String toString() {
         return symbol;
     }
 
-	// Function used for creating a cell given a string
-    static Cell fromSymbol(final String symbol) {
+    static Cell fromSymbol(String symbol) {
         Cell cellRepresentedBySymbol = null;
         for (Cell cell : Cell.values()) {
             if (cell.symbol.equals(symbol)) {
@@ -36,7 +32,6 @@ public enum Cell {
         return cellRepresentedBySymbol;
     }
 
-	// Default getter
     public String getSymbol() {
         return symbol;
     }
